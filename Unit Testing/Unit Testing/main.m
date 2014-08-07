9dfbdeab5647da3d98049c48341c59c0201a4462
dmiedema <daniel@househappy.org>
2014-08-06 19:30:49 -0700

    initial commit of project

---
+++ Unit Testing/Unit Testing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Unit Testing
+//
+//  Created by Portland Code School on 8/6/14.
+//  Copyright (c) 2014 Portland Code School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCSAppDelegate class]));
+    }
+}
