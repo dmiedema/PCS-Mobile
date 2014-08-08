5b57a134da87fbd6b3a1752e04ba8733baf48c82
Alex Reynolds <alexreynolds@househappy.org>
2014-08-08 10:45:31 -0700

    Added some comments

--- Fundamentals/Fundamentals/MyObject.h
+++ Fundamentals/Fundamentals/MyObject.h
@@ -8,9 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+// Strong properties increment the retain count of anything that is assigned to them.
+// If I store a string in a strong property and MyObject is released, then the retain count of the string is decremented.
+// If that string now has a retain count of 0, it is also released.
+
+// Weak properties do not increment the retain count.
+// If I make a string and assign it to the weak property and it is not stored anywhere else, then the retain count is still 0.
+// The string will be instantly released.
+// If I have object FOOBAR and I want a reference in my MyObject, I could assign it to a weak property. It will have a retain count of 1 since FOOBAR has it strong and MyObject has it weak. Releasing MyObject will not release the FOOBAR. Releasing FOOBAR will release the reference in MyObject.
+
 @interface MyObject : NSObject
 /// Public NSInteger property
 @property (nonatomic) NSInteger myInteger;
 /// Public string property.
-@property (copy, nonatomic) NSString *myString;
+@property (strong, nonatomic) NSString *myString;
+
+// weak reference where retain count is not incremented.
+@property (nonatomic, weak) id delegate;
 @end
